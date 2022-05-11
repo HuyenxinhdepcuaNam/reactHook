e4df835173a81b2c57515ad4d63f52d9a55e333c
Nam <ptnnamaxe@gmail.com>
2022-05-11 22:10:21 +0700

    video6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import money from './money.jpg'
+import Nav from './views/Nav';
 
 function App() {
   let link = 'https://zingnews.vn/'
 
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3 style={{ color: 'red' }}>Hello world with diiakhongchuidit</h3>
 
-        <img src={money} className="Moneyo" alt="" style={{ height: '300px' }} border={'10px solid yellow'} />
+        <img src={money} className="Moneyo" alt="" style={{ height: '300px' }} border={'10px solid yellow'} borderRadius={'100px'} />
 
         <a
           className="App-link"
