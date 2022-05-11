ce02d9257b79b210278e37078521f0acf6807b5c
Nam <ptnnamaxe@gmail.com>
2022-05-12 05:12:08 +0700

    video 8

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import money from './money.jpg'
 import Nav from './views/Nav';
+import { useState } from 'react';
+
 
 function App() {
-  let link = 'https://zingnews.vn/'
+  let [name, setName] = useState('diiakhongchuidit')
+  const [address, setAddress] = useState('')
 
   const handleEventClick = (event) => {
-    console.log('Click me', event.target.value)
+    setName(address)
   }
+  const handleOnchangeInput = (event) => {
+    setAddress(event.target.value)
 
+
+  }
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3 style={{ color: 'red' }}>Hello world with diiakhongchuidit</h3>
+        <h3 style={{ color: 'red' }}>Hello world with {name}</h3>
 
         <img src={money} className="Moneyo" alt="" style={{ height: '300px' }} border={'10px solid yellow'} />
-        <input type={'text'} value='sadfsd' />
+        <input type={'text'} value={address} onChange={(event) => handleOnchangeInput(event)} />
         <button
           type='button'
           onClick={(event) => handleEventClick(event)}
